cd1e0a944fcf2c34da7e1a62e28cd17403487313
Laura Cercas <laura.cercas@autentia.com>
2022-02-17 16:41:37 +0100

    Upgrade Aquimedes to 3.3.3 and small fixes

--- src/main/kotlin/micronaut/api/rest/kotlin/Application.kt
+++ src/main/kotlin/micronaut/api/rest/kotlin/Application.kt
@@ -1,15 +1,6 @@
 package micronaut.api.rest.kotlin
 
-
 import io.micronaut.runtime.Micronaut
-
-/*object Application {
-
-	@JvmStatic
-	fun main(args: Array<String>) {
-		Micronaut.run(Application.javaClass)
-	}
-}*/
 
 fun main(args: Array<String>) {
 	Micronaut.build()
